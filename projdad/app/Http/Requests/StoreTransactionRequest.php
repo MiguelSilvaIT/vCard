55c4e2cdc88a97767a45dca0a167184588cbd51e
Andre Costa <and.costa001@gmail.com>
2023-12-01 18:05:03 +0000

    requestStoreTransactions

--- projdad/app/Http/Requests/StoreTransactionRequest.php
+++ projdad/app/Http/Requests/StoreTransactionRequest.php
@@ -29,4 +29,25 @@ class StoreTransactionRequest extends FormRequest
             'spare_change' => 'nullable|boolean',
         ];
     }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     */
+    public function messages(): array
+    {
+        return [
+            'value.required' => 'O valor é obrigatório',
+            'value.numeric' => 'O valor tem de ser numérico',
+            'vcard.required' => 'O vcard é obrigatório',
+            'vcard.string' => 'O vcard tem de ser uma string',
+            'vcard.min' => 'O vcard tem de ter 9 caracteres',
+            'vcard.max' => 'O vcard tem de ter 9 caracteres',
+            'pair_vcard.required' => 'O vcard é obrigatório',
+            'pair_vcard.string' => 'O vcard tem de ser uma string',
+            'pair_vcard.min' => 'O vcard tem de ter 9 caracteres',
+            'pair_vcard.max' => 'O vcard tem de ter 9 caracteres',
+            'description.string' => 'A descrição tem de ser uma string',
+            'spare_change.boolean' => 'O spare_change tem de ser um booleano',
+        ];
+    }
 }
