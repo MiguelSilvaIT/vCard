7655633b617ae0b32d23021ab832a1086ce5c32a
Gonçalo Correia <2211017@my.ipleiria.pt>
2023-12-05 23:43:10 +0000

    vCard destroy

--- projdad/app/Http/Controllers/api/DADVcardController.php
+++ projdad/app/Http/Controllers/api/DADVcardController.php
@@ -10,29 +10,27 @@ use App\Models\Vcard;
 use Illuminate\Http\Request;
 use App\Services\Base64Services;
 use Illuminate\Support\Facades\Storage;
+use App\Models\Transaction;
+use App\Models\Category;
 
-class DADVcardController extends Controller
-{
-    public function index()
-    {
+class DADVcardController extends Controller {
+    public function index() {
         return VcardResource::collection(Vcard::all());
     }
 
-    private function storeBase64AsFile(Vcard $vcard, String $base64String)
-    {
+    private function storeBase64AsFile(Vcard $vcard, string $base64String) {
         $targetDir = storage_path('app/public/fotos');
-        $newfilename = $vcard->phone_number . "_" . rand(1000,9999);
+        $newfilename = $vcard->phone_number."_".rand(1000, 9999);
         $base64Service = new Base64Services();
         return $base64Service->saveFile($base64String, $targetDir, $newfilename);
     }
 
-    public function store(DADStoreVcardRequest $request)
-    {
+    public function store(DADStoreVcardRequest $request) {
         $dataToSave = $request->validated();
 
         $base64ImagePhoto = array_key_exists("base64ImagePhoto", $dataToSave) ?
-                $dataToSave["base64ImagePhoto"] : ($dataToSave["base64ImagePhoto"] ?? null);
-            unset($dataToSave["base64ImagePhoto"]);
+            $dataToSave["base64ImagePhoto"] : ($dataToSave["base64ImagePhoto"] ?? null);
+        unset($dataToSave["base64ImagePhoto"]);
 
 
         $newVcard = new Vcard();
@@ -45,7 +43,7 @@ class DADVcardController extends Controller
         $newVcard->balance = 0;
         $newVcard->max_debit = 5000;
 
-        if ($base64ImagePhoto) {
+        if($base64ImagePhoto) {
             $newVcard->photo_url = $this->storeBase64AsFile($newVcard, $base64ImagePhoto);
         }
 
@@ -58,14 +56,12 @@ class DADVcardController extends Controller
         ], 200);
     }
 
-    public function show($phoneNumber)
-    {
+    public function show($phoneNumber) {
         $vcard = Vcard::findOrFail($phoneNumber);
         return new VcardResource($vcard);
     }
 
-    public function update(DADUpdateVcardRequest $request, $phoneNumber)
-    {
+    public function update(DADUpdateVcardRequest $request, $phoneNumber) {
         $vcard = Vcard::findOrFail($phoneNumber);
         $dataToSave = $request->validated();
 
@@ -81,15 +77,15 @@ class DADVcardController extends Controller
         $vcard->confirmation_code = $dataToSave['confirmation_code'];
 
         // Delete previous photo file if a new file is uploaded or the photo is to be deleted
-        if ($vcard->photo_url && ($deletePhotoOnServer || $base64ImagePhoto)) {
-            if (Storage::exists('public/fotos/' . $vcard->photo_url)) {
-                Storage::delete('public/fotos/' . $vcard->photo_url);
+        if($vcard->photo_url && ($deletePhotoOnServer || $base64ImagePhoto)) {
+            if(Storage::exists('public/fotos/'.$vcard->photo_url)) {
+                Storage::delete('public/fotos/'.$vcard->photo_url);
             }
             $vcard->photo_url = null;
         }
 
         // Create a new photo file from base64 content
-        if ($base64ImagePhoto) {
+        if($base64ImagePhoto) {
             $vcard->photo_url = $this->storeBase64AsFile($vcard, $base64ImagePhoto);
         }
         $vcard->save();
@@ -102,11 +98,10 @@ class DADVcardController extends Controller
 
 
 
-    public function destroy($phoneNumber)
-    {
+    public function destroy($phoneNumber) {
         $vcard = Vcard::findOrFail($phoneNumber);
 
-        if ($vcard->balance != 0) {
+        if($vcard->balance != 0) {
             return response()->json([
                 'message' => 'error',
                 'data' => 'Vcard has balance'
@@ -114,18 +109,33 @@ class DADVcardController extends Controller
         }
         //confirmar se o vCard tem transações associads
         //se tiver, apagar com soft delete:
-        //Vcard::destroy($phoneNumber);
 
-        //se não tiver, apagar com hard delete:
-        $vcard->forceDelete();
-        return response()->json([
-            'message' => 'success',
-            'data' => new VcardResource($vcard)
-        ], 200);
+        $transactions = Transaction::where('vcard', $phoneNumber)->get();
+        $categories = Category::where('vcard', $phoneNumber)->get();
+        if($transactions || $categories) {
+            foreach($categories as $category) {
+                $category->delete();
+            }
+            foreach($transactions as $transaction) {
+                $transaction->delete();
+            }
+            $vcard->delete();
+            return response()->json([
+                'message' => 'success',
+                'data' => new VcardResource($vcard),
+            ], 200);
+        } else {
+            //se não tiver, apagar com hard delete:
+            $vcard->forceDelete();
+            return response()->json([
+                'message' => 'success',
+                'data' => new VcardResource($vcard)
+            ], 200);
+        }
+
     }
 
-    public function show_me(Request $request)
-    {
+    public function show_me(Request $request) {
         return new VcardResource($request->vcard());
     }
 }
