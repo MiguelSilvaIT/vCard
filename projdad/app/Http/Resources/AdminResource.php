192bb2538383bafed8734083d87b1d230ad1ad24
Andre Costa <and.costa001@gmail.com>
2023-12-13 21:23:44 +0000

    admin resource add id

--- projdad/app/Http/Resources/AdminResource.php
+++ projdad/app/Http/Resources/AdminResource.php
@@ -16,6 +16,7 @@ class AdminResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
             'password' => $this->password,
